a322b23feb0cdcc3b2f822abc69e6a22f672989e
Igor <makowskiigor@gmail.com>
2022-08-24 19:28:16 +0200

    feat(app): implement resolver

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -4,14 +4,25 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { AppResolver } from './app.resolver'
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: true,
       driver: ApolloDriver,
+      playground: {
+        settings: {
+          'request.credentials': 'include',
+        },
+      },
+      cors: {
+        credentials: true,
+        origin: true,
+      },
     }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AppResolver],
 })
 export class AppModule {}
